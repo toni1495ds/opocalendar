4b2fd71ae7895a979cba58eef9ef7397788ecb9a
tonisanmartin-ranger <toni@ranger.net>
2026-07-30 14:38:11 -0700

    Configura el projecte real de Firebase (opocalendar-3ef5a)

--- js/config.js
+++ js/config.js
@@ -1,12 +1,12 @@
 // ============ CONFIGURACIÓ FIREBASE ============
 // >>> ENGANXA AQUÍ la teva configuració de Firebase (la trobes a la guia, pas 4) <<<
 export const firebaseConfig = {
-  apiKey: "ENGANXA_AQUI",
-  authDomain: "ENGANXA_AQUI",
-  projectId: "ENGANXA_AQUI",
-  storageBucket: "ENGANXA_AQUI",
-  messagingSenderId: "ENGANXA_AQUI",
-  appId: "ENGANXA_AQUI"
+  apiKey: "AIzaSyCVwexBJ0GSSISsMlEx_-bFVCWfzl6yPV8",
+  authDomain: "opocalendar-3ef5a.firebaseapp.com",
+  projectId: "opocalendar-3ef5a",
+  storageBucket: "opocalendar-3ef5a.firebasestorage.app",
+  messagingSenderId: "828089349640",
+  appId: "1:828089349640:web:78674d3bc7f737b3fdc3bd"
 };
 
 // >>> CODI D'ACCÉS: canvia'l pel que vulguis (mínim recomanat 4 dígits) <<<
